6c424f0992169d4d9e02c2858189d91bc6d1654f
Sveto <37042480+sveto8@users.noreply.github.com>
2026-09-02 14:49:44 +0200

    Update installApps.sh

--- installApps.sh
+++ installApps.sh
@@ -83,7 +83,6 @@ AUR_PACKAGES=(
 echo
 read "Install additional apps"
 echo
-echo
 read -r -p "Install snx-rs (Check Point VPN client, AUR, slow build)? [y/N] " ANSWER
 if [[ "$ANSWER" =~ ^[Yy]$ ]]; then
     AUR_PACKAGES+=(snx-rs)
