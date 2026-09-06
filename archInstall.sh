68d49cb76600d4a40480c93173d42165793d3691
Sveto <37042480+sveto8@users.noreply.github.com>
2026-09-06 18:19:27 +0200

    Update archInstall.sh

--- archInstall.sh
+++ archInstall.sh
@@ -368,8 +368,7 @@ HOOKS=(base systemd autodetect microcode modconf kms keyboard sd-vconsole block 
 HOOKS_EOF
 mkinitcpio -P
 
-# Ispravljena linija - koristi dvostruke navodnike
-sed -i -E "s/^GRUB_CMDLINE_LINUX_DEFAULT=.*/GRUB_CMDLINE_LINUX_DEFAULT=\"rd.luks.name=${LUKS_UUID}=cryptroot root=\/dev\/mapper\/cryptroot rootflags=subvol=@ quiet\"/" /etc/default/grub
+sed -i -E "s/^GRUB_CMDLINE_LINUX_DEFAULT=.*/GRUB_CMDLINE_LINUX_DEFAULT=\"rd.luks.name=${LUKS_UUID}=cryptroot root=\/dev\/mapper\/cryptroot rootflags=subvol=@ quiet rd.vconsole.keymap=us rd.locale.LANG=en_US.UTF-8\"/" /etc/default/grub
 
 grub-install --target=x86_64-efi --efi-directory=/efi --boot-directory=/boot --bootloader-id=GRUB --recheck --removable
 grub-mkconfig -o /boot/grub/grub.cfg
